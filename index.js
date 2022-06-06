4e7abe8c6f9414d4c04ad21b4b9aed50d9ce6cf0
djiby thioub <thioubdjiby96@gmail.com>
2022-06-06 21:43:17 +0000

    color flipper

--- index.js
+++
@@ -1,9 +0,0 @@
-//let declare a variable
-let number =1
-for(let i=0;i<10;i++){
-    console.log(number)
-    number++
-}
-function add(a,b){
-    return a+b
-}
---
+++ js/index.js
@@ -0,0 +1,5 @@
+//let declare a variable
+let number =1
+number=2
+console.log(number)
+alert(number)
